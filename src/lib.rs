c181043c9e9db4bd5e1fd749b4fbd3fff52d2f0c
equation-man <bigtechguyz@gmail.com>
2026-03-12 08:24:16 +0300

    Entrypoint fixed

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,9 @@ use instructions::{
 declare_id!("DnWWkqtWVwv5bVc4mnnvxMvZZUsuYNCpZQHGPixbqm4v");
 entrypoint!(process_instruction);
 fn process_instruction(_program_id: &Address, accounts: &[AccountView], instruction_data: &[u8]) -> ProgramResult {
-    log!("The flash loan entrypoint");
-    Ok(())
-    //match instruction_data.split_first() {
-    //    Some((Loan::DISCRIMINATOR, data)) => Loan::try_from((data, accounts))?.process(),
-    //    Some((Repay::DISCRIMINATOR, _)) => Repay::try_from(accounts)?.process(),
-    //    _ => Err(ProgramError::InvalidInstructionData)
-    //}
+    match instruction_data.split_first() {
+        Some((Loan::DISCRIMINATOR, data)) => Loan::try_from((data, accounts))?.process(),
+        Some((Repay::DISCRIMINATOR, _)) => Repay::try_from(accounts)?.process(),
+        _ => Err(ProgramError::InvalidInstructionData)
+    }
 }
